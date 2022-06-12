92a584a577ca1d21c720f9f99536a106517b969e
Hakan Sarıdiken <84566528+hakansdiken@users.noreply.github.com>
2022-06-12 22:49:21 +0300

    ListNode

---
+++ week10/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        LinkedIntList linklist = new LinkedIntList();
+        linklist.add(5);
+        linklist.add(7);
+        linklist.add(9);
+        System.out.println(linklist.toString());
+        //linklist.remove(2);
+        //System.out.println(linklist.toString());
+        linklist.add(0,11);
+        System.out.println(linklist.toString());
+        linklist.add(2,15);
+        System.out.println(linklist.toString());
+        int count = linklist.size();
+        System.out.println(count);
+    }
+}
